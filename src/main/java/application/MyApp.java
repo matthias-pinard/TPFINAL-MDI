4741262b950550ad3bfaab43d6803c458bc328a1
Matthias <matthiaspinard@gmail.com>
2019-05-21 17:17:58 +0200

    cors

--- src/main/java/application/MyApp.java
+++ src/main/java/application/MyApp.java
@@ -1,17 +1,34 @@
 package application;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 import javax.ws.rs.core.Application;
 
 
+import org.jboss.resteasy.plugins.interceptors.CorsFilter;
 import rest.Rest;
 
 public class MyApp extends Application{
+	Set<Object> singletons;
+
 	@Override
     public Set<Class<?>> getClasses() {
 		Set<Class<?>> set = new HashSet<Class<?>>();
 		set.add(Rest.class);
 		return set;
-	}}
+	}
+
+	@Override
+	public Set<Object> getSingletons() {
+		if (singletons == null) {
+			CorsFilter corsFilter = new CorsFilter();
+			corsFilter.getAllowedOrigins().add("*");
+
+			singletons = new LinkedHashSet<Object>();
+			singletons.add(corsFilter);
+		}
+		return singletons;
+	}
+}
